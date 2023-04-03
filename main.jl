3ecfbbea299a2f1f4a1dd0f368d5561f7b68aad7
bryanegbert561@gmail.com <bryanegbert561@gmail.com>
2023-04-04 00:15:53 +0700

    create model pipeline

--- main.jl
+++ main.jl
@@ -12,48 +12,71 @@ using Plots
 
 df = CSV.File(open("sentiment_tweets3.csv", enc"ISO-8859-1")) |> DataFrame
 rename!(df, [:Id, :Tweet, :Target])
-# df = CSV.read("sentiment_tweets3.csv", DataFrames.DataFrame; header=[:Target, :Id, :Date, :Flag, :User, :Tweet])
 
-Plots.bar(["0", "1"], )
-tweet_string_docs = TextAnalysis.StringDocument.(df[:, :Tweet])
+schema(df)
+
+# tweet_string_docs = TextAnalysis.StringDocument.(df[:, :Tweet])
+df.Target = coerce(df.Target, OrderedFactor)
+levels(df.Target) 
+
+df.Tweet = TextAnalysis.StringDocument.(df[:, :Tweet])
+
+
+schema(df)
+
+feat, target = MLJ.unpack(df, ==(:Tweet), ==(:Target))
 
 # remove_case!.(tweet_string_docs)
-prepare!.(tweet_string_docs, strip_case| strip_punctuation| strip_numbers| strip_non_letters| strip_pronouns| strip_stopwords| stem_words)
+prepare!.(feat, strip_case| strip_punctuation| strip_numbers| strip_non_letters| strip_pronouns| strip_stopwords| stem_words)
 
-crps = Corpus(tweet_string_docs)
+crps = Corpus(feat)
 
 update_lexicon!(crps)
-
-tweet_string_docs =  tokenize.(TextAnalysis.text.(tweet_string_docs))
-sizeof(tweet_string_docs)
-
-tf_idf_transformer = TfidfTransformer()
-mach = machine(tf_idf_transformer, tweet_string_docs; cache=false) |> fit!
-
-fitted_params(mach)
-
-tfdif_mat = MLJ.transform(mach, tweet_string_docs)
-
-
-feat, target = round.(tfdif_mat), df[:, :Target]
-
-MultinomialNBClassifier = @load MultinomialNBClassifier pkg=NaiveBayes
-
-model = MultinomialNBClassifier()
-
-mach = machine(model, coerce(feat, Count), coerce(target, Finite); cache=false)
 
 rng = StableRNG(100)
 train, test = partition(eachindex(target), 0.7, shuffle=true, rng=rng);
 
+MultinomialNBClassifier = @load MultinomialNBClassifier pkg=NaiveBayes
+pipe = (feat -> tokenize.(TextAnalysis.text.(feat))) |> TfidfTransformer() |> (feat -> round.(feat)) |> MultinomialNBClassifier()
+
+mach = machine(pipe, feat, target)
 MLJ.fit!(mach, rows=train)
+fitted_params(mach)
 
-probability = MLJ.predict(mach, rows=test)
+evaluate!(mach, resampling=CV(), measure=[Accuracy()])
 
-println("Log loss on the test set of index 1-10: $(log_loss(probability, target[test]) |> mean)")
+yhat = MLJ.predict(pipe, rows=test)
 
-println("Accuracy on the test set of index 1-10: $(accuracy(mode.(probability), target[test]))")
+# tweet_string_docs =  tokenize.(TextAnalysis.text.(tweet_string_docs))
 
-confusion_mat = ConfusionMatrix()(mode.(probability), coerce(target[test], OrderedFactor))
-cm_plt = Plots.heatmap(confusion_mat.mat, xlabel="ground truth", ylabel="predicted values", title="Confusion Matrix of\nMNB Classifier using Rounded TF-IDF Vectorizer", size= (800, 800))
+# tf_idf_transformer = TfidfTransformer()
+# tfidf = machine(tf_idf_transformer, source(tweet_string_docs)) |> MLJ.fit!
+
+# fitted_params(tfidf)
+
+# tfidf_mat = MLJ.transform(tfidf, tweet_string_docs)
+
+# feat, target = round.(tfidf_mat), df[:, :Target]
+
+# rng = StableRNG(100)
+# train, test = partition(eachindex(target), 0.7, shuffle=true, rng=rng);
+
+# MultinomialNBClassifier = @load MultinomialNBClassifier pkg=NaiveBayes
+
+# model = MultinomialNBClassifier()
+
+# mach = machine(model, coerce(feat, Count), coerce(target, Finite); cache=false)
+
+# MLJ.fit!(mach, rows=train)
+# fitted_params = fitted_params(mach)
+
+# probability = MLJ.predict(mach, rows=test)
+
+# println("Log loss on the test set: $(log_loss(probability, target[test]) |> mean)")
+
+# println("Accuracy on the test set: $(MLJ.accuracy(mode.(probability), target[test]))")
+# println("fscore on the test set: $(MLJ.f1score(mode.(probability), target[test]))")
+
+# confusion_mat = ConfusionMatrix()(mode.(probability), coerce(target[test], OrderedFactor))
+# cm_plt = Plots.heatmap(confusion_mat.mat, xlabel="ground truth", ylabel="predicted values", title="Confusion Matrix of\nMNB Classifier using Rounded TF-IDF Vectorizer", size= (800, 800))
 
